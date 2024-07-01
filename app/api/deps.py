680d432a294b0197170be0b03df402c3cd308347
SerHappy <iamserhappy@yandex.ru>
2024-07-01 17:34:16 +0300

    Add OAuth2PasswordBearer deps

--- app/api/deps.py
+++ app/api/deps.py
@@ -2,8 +2,21 @@ from collections.abc import AsyncGenerator
 from typing import Annotated
 
 from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
 
+from app.core.config import settings
 from app.infra.uow import SQLALchemyUnitOfWork
+
+reusable_oauth2 = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.SECURITY.DOMAIN}/auth/access-token",
+)
+optional_oauth2 = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.SECURITY.DOMAIN}/auth/access-token",
+    auto_error=False,
+)
+
+TokenDep = Annotated[str, Depends(reusable_oauth2)]
+OptionalTokenDep = Annotated[str | None, Depends(optional_oauth2)]
 
 
 async def get_uow() -> AsyncGenerator[SQLALchemyUnitOfWork, None]:
